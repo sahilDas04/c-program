28759002e31a5d64d4c77696a8d9dabdd23f0bd6
pratyush singha <pratyushsingha83@gmail.com>
2022-09-18 20:14:56 +0530

    Create starpattern3.c

---
+++ star-pattern/starpattern3.c
@@ -0,0 +1,32 @@
+/* write a c program to print the following star pattern for a generalized value of n;
+    *
+   **
+  ***
+ ****
+
+   */
+
+#include <stdio.h>
+int main()
+{
+	int n;
+	printf("Enter the no of rows\n");
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (i + j >= n - 1)
+			{
+				printf("*");
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
